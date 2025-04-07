477c0b6622c27de2a852d49542c22a29513f1d2c
Alonso8729 <alon8729@gmail.com>
2025-04-07 13:48:03 +0300

    Added fallback value for mongo_uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UserModule],
+imports: [AuthModule, UserModule, MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/parker'), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
