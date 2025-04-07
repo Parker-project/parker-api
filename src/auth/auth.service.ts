ae739cf8fe75d5da90108cc9a60968d1803676a7
Alonso8729 <alon8729@gmail.com>
2025-04-07 13:15:01 +0300

    Implement the basic of signup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Model } from 'mongoose';
+import { User, UserDocument } from '../user/user.schema'
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
+
 
 @Injectable()
 export class AuthService {
-    signup(createUserDTO: CreateUserDto) {
-        return;
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>
+    ) { }
+    async signup(createUserDto: CreateUserDto) {
+        const existingUser = await this.userModel.find({ email: createUserDto.email })
+
+        if (existingUser) {
+            throw new ConflictException('Email address is already in use')
+        }
+
+        const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+        const verificationToken = randomBytes(32).toString('hex');
+
+        const user = await this.userModel.create({
+            email: createUserDto.email,
+            firstName: createUserDto.firstName,
+            lastName: createUserDto.lastName,
+            password: hashedPassword,
+            verificationToken
+        })
+        
+        // Send Email verification 
+        
+        return { message: 'Check your email to verify your account' };
     }
 }
