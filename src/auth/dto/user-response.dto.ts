63dbb30eab93f2aaf021b3430ec6302dd95f97cd
Alonso8729 <alon8729@gmail.com>
2025-04-08 19:50:25 +0300

    Added validators

--- src/auth/dto/user-response.dto.ts
+++ src/auth/dto/user-response.dto.ts
@@ -1,5 +1,10 @@
+import { IsBoolean, IsEmail, IsString } from "class-validator";
+
 export class UserResponseDto {
+    @IsString()
     id: string;
+    @IsEmail()
     email: string;
+    @IsBoolean()
     isEmailVerified: boolean;
 }
