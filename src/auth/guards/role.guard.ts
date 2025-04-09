cb3a06457741eda535e5094bc48f433b300a78c2
Alonso8729 <alon8729@gmail.com>
2025-04-09 21:19:56 +0300

    Implemented role guard

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/role.decorator';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ])
+
+        if (!requiredRoles) {
+            return true;
+        }
+
+        const request = context.switchToHttp().getRequest()
+        const user = request.user;
+
+        if (!user || !requiredRoles.includes(user.role)) {
+            throw new ForbiddenException('Access Denied');
+        }
+        return true;
+    }
+
+}
