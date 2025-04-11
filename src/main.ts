6a571cbd5e266b127dbf720ed789a22dc8055ee1
Alonso8729 <alon8729@gmail.com>
2025-04-11 13:02:24 +0300

    Added cookie parser middleware

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+
 
 import 'dotenv/config';
 
@@ -8,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
+  app.use(cookieParser());
   await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
