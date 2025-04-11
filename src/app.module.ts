b7ab3200d7e92e35576f89525ee62abeb4e11d32
Alonso8729 <alon8729@gmail.com>
2025-04-11 12:45:58 +0300

    Added isGlobal flag for env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-imports: [AuthModule, UserModule, MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/parker'), 
+imports: [AuthModule, UserModule, MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/parker'),
+  ConfigModule.forRoot({
+    isGlobal: true, 
+    envFilePath: '.env', 
+  }) 
   ],
   controllers: [AppController],
   providers: [AppService],
