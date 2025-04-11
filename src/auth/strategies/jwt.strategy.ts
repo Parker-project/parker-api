2336924ce9dbf76a5bb4ecef5a647cc4480d95d9
Alonso8729 <alon8729@gmail.com>
2025-04-11 13:05:58 +0300

    Added flags

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,30 +1,24 @@
 // src/auth/strategies/jwt.strategy.ts
-
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User, UserDocument } from 'src/user/user.schema';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private configService: ConfigService,
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
-  ) {
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req) => req?.cookies?.access_token,   
+        (req) => {
+          return req?.cookies?.['access_token'];
+        },
       ]),
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'), 
     });
   }
 
-  async validate(payload: { sub: string; role: string }) {
-    const user = await this.userModel.findById(payload.sub);
-    if (!user) throw new UnauthorizedException('Invalid token');
-    return user;  
+  async validate(payload: any) {
+    return { id: payload.sub, role: payload.role };
   }
 }
